fb619ff2b5eee11455fed0d7ba18518b691c9fab
aronalxd <55291528+aronalxd@users.noreply.github.com>
2023-07-20 11:33:39 -0500

    Create saludo.js

---
+++ saludo.js
@@ -0,0 +1,10 @@
+{
+  "name": "mi-primer-proyecto-node",
+  "version": "1.0.0",
+  "description": "Mi primer proyecto de Node en el mejor Bootcamp de programación del mundo",
+  "scripts": {
+    "saludo": "echo 'Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo'"
+  },
+  "author": "Tu Nombre",
+  "license": "MIT"
+}
